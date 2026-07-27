8dee084c30c8f9d5fd5bc53a884a7991f3151ca2
martinzico35-afk <martinzico35@gmail.com>
2026-07-27 12:40:54 +0100

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization script.

--- firebase-config.js
+++ firebase-config.js
@@ -42,3 +42,26 @@ function initFirebase() {
         return false;
     }
 }
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCS-AWV0IoidI-RTzThHA7c5H17s-hCESM",
+    authDomain: "zeepredict.firebaseapp.com",
+    projectId: "zeepredict",
+    storageBucket: "zeepredict.firebasestorage.app",
+    messagingSenderId: "305238449603",
+    appId: "1:305238449603:web:e3116dadf5c1ce0a9fb176",
+    measurementId: "G-EPMPJSEQMW"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
